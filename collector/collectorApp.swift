1c86028197f63ca05e3f3e88661ab5cab4d47c28
khoi <git@khoi.io>
2023-10-28 13:53:49 +0700

    Initial Commit

---
+++ collector/collectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  collectorApp.swift
+//  collector
+//
+//  Created by khoi on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct collectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
